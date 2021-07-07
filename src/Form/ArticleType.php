03dbb0c5bf779f8775bb018ce4f2cfaf8561f2a4
Ceciliagr <grene_c@etna-alternance.net>
2021-07-07 18:13:17 +0200

    Ajout article

---
+++ src/Form/ArticleType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            // ne j'mais utilisé le doctrine/Dbal
+            ->add('nom', TextType::class, [
+                'required'=>false,
+                'label'=>false,
+                'attr'=>[
+                    'placeolder'=>'Veuillez saisir le nom de l\'article',
+                ]
+            ])
+            ->add('prix', NumberType::class,[
+                'required'=>false,
+                'label'=>false,
+                'attr'=>[
+                    'placeolder'=>'Veuillez saisir le prix de l\'article',
+                ]
+            ])
+            ->add('photo', FileType::class, [
+                'required'=>false,
+                'label'=>false,
+            ])
+
+            ->add('Valider', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
